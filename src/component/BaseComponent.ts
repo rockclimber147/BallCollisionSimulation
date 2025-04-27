4b1f59e4ecf1c665c9dad717ce67df3659b70890
Daylen Smith <daylencsmith@gmail.com>
2025-04-26 19:27:48 -0700

    refactor ComponentBase to allow better interComponent communication

--- src/component/BaseComponent.ts
+++ src/component/BaseComponent.ts
@@ -3,31 +3,8 @@ interface Observer {
   update(): void;
 }
 
-interface Subject {
-  addObserver(observer: Observer): void;
-  removeObserver(observer: Observer): void;
-  notify(notificationType: string): void;
-}
-
-interface ComponentUI<T extends ComponentModelBase> {
-  model: T;
-  setup(): void;
-  tearDown(): void;
-  inject(targetId: string): void;
-}
-
-abstract class ComponentUIBase<T extends ComponentModelBase> implements Observer, ComponentUI<T> {
-  model: T;
+abstract class ObserverBase implements Observer {
   private actions: Map<string, () => void> = new Map();
-  protected container?: HTMLElement;
-
-  abstract setup(): void;
-  abstract tearDown(): void;
-
-  constructor(model: T) {
-    this.model = model;
-  }
-
   updateWithType(notificationType: string): void {
     const action = this.actions.get(notificationType);
     if (!action) {
@@ -41,14 +18,6 @@ abstract class ComponentUIBase<T extends ComponentModelBase> implements Observer
     for (const action of this.actions.values()) action();
   }
 
-  inject(targetId: string): void {
-    const target = document.getElementById(targetId);
-    if (!target) throw new Error(`Target element with id "${targetId}" not found.`);
-    if (!this.container) throw new Error(`No container to inject for UI component.`);
-
-    target.appendChild(this.container);
-  }
-
   addAction(type: string, action: () => void) {
     this.actions.set(type, action);
   }
@@ -58,8 +27,14 @@ abstract class ComponentUIBase<T extends ComponentModelBase> implements Observer
   }
 }
 
-abstract class ComponentModelBase implements Subject {
-  observers: Observer[] = [];
+interface Subject {
+  addObserver(observer: Observer): void;
+  removeObserver(observer: Observer): void;
+  notify(notificationType: string): void;
+}
+
+class SubjectBase implements Subject {
+  private observers: Observer[] = [];
   addObserver(observer: Observer): void {
     this.observers.push(observer);
   }
@@ -71,17 +46,69 @@ abstract class ComponentModelBase implements Subject {
   }
 }
 
-abstract class ComponentBase<M extends ComponentModelBase, U extends ComponentUIBase<M>> {
-  readonly model: M;
-  readonly ui: U;
-  readonly targetId: string;
+interface ComponentUI<T extends ComponentModelBase> {
+  model: T;
+  setup(): void;
+  tearDown(): void;
+  inject(targetId: string): void;
+}
+
+abstract class ComponentUIBase<T extends ComponentModelBase>
+  extends ObserverBase
+  implements ComponentUI<T>
+{
+  model: T;
+  protected container?: HTMLElement;
+
+  abstract setup(): void;
+  abstract tearDown(): void;
+
+  constructor(model: T) {
+    super();
+    this.model = model;
+  }
+
+  inject(targetId: string): void {
+    const target = document.getElementById(targetId);
+    if (!target) throw new Error(`Target element with id "${targetId}" not found.`);
+    if (!this.container) throw new Error(`No container to inject for UI component.`);
+
+    target.appendChild(this.container);
+  }
+}
+
+abstract class ComponentModelBase extends SubjectBase {
+
+}
+
+abstract class ComponentBase<M extends ComponentModelBase, U extends ComponentUIBase<M>>
+  extends ObserverBase
+  implements Subject
+{
+  protected readonly model: M;
+  protected readonly ui: U;
+  protected readonly targetId: string;
+  private readonly subject = new SubjectBase()
 
   constructor(model: M, ui: U, targetId: string) {
+    super();
     this.model = model;
     this.ui = ui;
     this.targetId = targetId;
 
     this.model.addObserver(this.ui);
+  }
+
+  addObserver(observer: Observer): void {
+    this.subject.addObserver(observer);
+  }
+
+  removeObserver(observer: Observer): void {
+    this.subject.removeObserver(observer);
+  }
+
+  notify(notificationType: string): void {
+    this.subject.notify(notificationType);
   }
 
   setup(): void {
