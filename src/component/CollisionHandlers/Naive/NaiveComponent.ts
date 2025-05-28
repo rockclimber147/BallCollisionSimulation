c600f586035fe46004bdd1d1ee1fbe9d3abe4545
Daylen Smith <daylencsmith@gmail.com>
2025-05-28 14:20:14 -0700

    Make a naive collision handler component

---
+++ src/component/CollisionHandlers/Naive/NaiveComponent.ts
@@ -0,0 +1,59 @@
+import { PhysicsBall } from '../../../ball_physics/Ball.js';
+import { Drawable } from '../../../display/Drawable.js';
+import { ComponentUIBase } from '../../BaseComponent.js';
+import {
+  BallCollisionPair,
+  CollisionHandler,
+  CollisionHandlerComponentBase,
+  CollisionHandlerModelBase,
+} from '../CollisionHandler.js';
+
+class NaiveUI extends ComponentUIBase {
+  async setup(): Promise<void> {
+    this.container = await this.loadTemplate(import.meta.url);
+  }
+
+  tearDown(): void {
+    this.container?.remove();
+  }
+}
+
+class NaiveModel extends CollisionHandlerModelBase {
+  getAllPotentialCollisions(balls: PhysicsBall[]): BallCollisionPair[] {
+    const pairs: BallCollisionPair[] = [];
+    for (let i = 0; i < balls.length; i++) {
+      for (let j = i + 1; j < balls.length; j++) {
+        pairs.push(new BallCollisionPair(balls[i], balls[j]));
+      }
+    }
+    return pairs;
+  }
+  getCollisionRepresentation(): Drawable[] {
+    return [];
+  }
+}
+
+export class NaiveComponent
+  extends CollisionHandlerComponentBase<NaiveModel, NaiveUI>
+  implements CollisionHandler
+{
+  setupChildren(): Promise<void> {
+    return Promise.resolve();
+  }
+  tearDownChildren(): void {
+    return;
+  }
+  constructor(targetID: string) {
+    super(new NaiveModel('Naive'), new NaiveUI(), targetID);
+  }
+  setupUIEvents(): void {
+    return;
+  }
+
+  getAllPotentialCollisions(balls: PhysicsBall[]): BallCollisionPair[] {
+    return this.model.getAllPotentialCollisions(balls);
+  }
+  getCollisionRepresentation(): Drawable[] {
+    return this.model.getCollisionRepresentation();
+  }
+}
